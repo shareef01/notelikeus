25e2b5ca901bb69aaedab908cd0939f62ae534b7
shareef01 <shareef.q13@gmail.com>
2026-08-18 01:10:26 +0200

    Release 1.0.1

--- composeApp/src/androidMain/kotlin/com/aus/notelikeus/util/AppConfig.android.kt
+++ composeApp/src/androidMain/kotlin/com/aus/notelikeus/util/AppConfig.android.kt
@@ -4,7 +4,7 @@ import com.aus.notelikeus.shared.BuildConfig
 
 actual object AppConfig {
     actual val isDebug: Boolean = BuildConfig.DEBUG
-    actual val versionName: String = "1.0.0" // Or get from BuildConfig if defined
+    actual val versionName: String = "1.0.1" // Or get from BuildConfig if defined
     actual val supportsAppLock: Boolean = true
     actual val supportsCloudSync: Boolean = true
     actual val isDesktop: Boolean = false
